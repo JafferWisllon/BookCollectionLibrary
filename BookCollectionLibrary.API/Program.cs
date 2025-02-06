82bd43e803a0528cb1c21a8c31f1cabe153cd90b
Jaffer Wisllon <contatojaffer@gmail.com>
2025-02-06 20:22:22 -0300

    chore: add migrations and seeds

--- BookCollectionLibrary.API/Program.cs
+++ BookCollectionLibrary.API/Program.cs
@@ -3,7 +3,14 @@ using BookCollectionLibrary.API.Business.Implementations;
 using BookCollectionLibrary.API.Model.Context;
 using BookCollectionLibrary.API.Repository;
 using BookCollectionLibrary.API.Repository.Implementations;
+using EvolveDb;
 using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +25,11 @@ builder.Services.AddDbContext<MySQLContext>(options =>
 {
     options.UseMySql(connection, serverVersion);
 });
+
+if (builder.Environment.IsDevelopment())
+{
+    MigrationDatabase(connection);
+}
 
 builder.Services.AddApiVersioning();
 
@@ -37,3 +49,22 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void MigrationDatabase(string connection)
+{
+    try
+    {
+        var evolveConnection = new MySqlConnection(connection);
+        var evolve = new Evolve(evolveConnection, Log.Information)
+        {
+            Locations = new List<string> { "db/migrations", "db/dataset" },
+            IsEraseDisabled = true,
+        };
+        evolve.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Log.Error("Database migration failed", ex);
+        throw;
+    }
+}
--- BookCollectionLibrary.API/Repository/Implementations/PersonRepositoryImplementation.cs
+++ BookCollectionLibrary.API/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -37,7 +37,7 @@ public class PersonRepositoryImplementation : IPersonRepository
 
     public Person Update(Person person)
     {
-        if (!Exists(person.Id)) return new Person();
+        if (!Exists(person.Id)) return null;
 
         var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
         if (result != null)
